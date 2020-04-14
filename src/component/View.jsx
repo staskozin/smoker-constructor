1d816e0c90a11518914f243344379b92cbd4ed04
Stanislav Kozin <stas@staskozin.ru>
2020-04-15 00:31:02 +0300

    Update View component

--- src/component/View.jsx
+++ src/component/View.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import s from './View.scss';
 
-export default class From extends React.Component {
+export default class View extends React.Component {
   render() {
     return (
       <>
-        <img className={`${s.hook} img-responsive`} src="img/states/kruk_low.png" alt="Крючки для подвешивания продуктов" />
+        <img className={`${s.hook} img-responsive`} src="img/states/hook_low.png" alt="Крючки для подвешивания продуктов" />
         <img className={`img-responsive`} src="img/states/10000.jpg" alt="10000" />
       </>
     )
